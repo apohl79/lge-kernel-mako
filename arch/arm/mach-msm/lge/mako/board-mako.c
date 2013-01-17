d3a401f41e65cb18945999a12c8c7dcbf152a7e4
franciscofranco <franciscofranco.1990@gmail.com>
2013-01-16 23:28:25 -0800

    msm: dcvs: change tz buffer to 32KB - https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git;a=commitdiff;h=3edb5de258e187af999aafc35dc9588d980d3cbf
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Andreas Pohl <apohl79@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -609,6 +609,11 @@ static void __init reserve_ion_memory(void)
 #endif
 }
 
+static void __init reserve_mpdcvs_memory(void)
+{
+	apq8064_reserve_table[MEMTYPE_EBI1].size += SZ_32K;
+}
+
 static void __init reserve_mdp_memory(void)
 {
 	apq8064_mdp_writeback(apq8064_reserve_table);
@@ -633,6 +638,7 @@ static void __init apq8064_calculate_reserve_sizes(void)
 	reserve_mdp_memory();
 	reserve_rtb_memory();
 	reserve_cache_dump_memory();
+	reserve_mpdcvs_memory();
 }
 
 static struct reserve_info apq8064_reserve_info __initdata = {
